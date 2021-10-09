39563ca0c19fe3cd44385d59cbd95af66951fa1d
jobinben <1021137079@qq.com>
2021-10-09 21:04:43 +0800

    优化代码

--- navDemo/nav.js
+++ navDemo/nav.js
@@ -1,3 +1,36 @@
+
+/**
+ * 获取本地缓存
+ * @param {string} name 
+ * @returns 缓存数据
+ */
+function getFromLocalStorage(name) {
+    return JSON.parse(localStorage.getItem(name) || null)
+}
+
+/**
+ * 创建元素
+ * @param {string} tagName 标签名称
+ * @param {object} attribute 需要添加的属性
+ * @returns 元素
+ */
+function createTag(tagName, attribute = {}) {
+    let element = document.createElement(tagName)
+    for(let key in attribute) {
+        element[key] = attribute[key]
+    }
+    return element
+}
+
+// 编辑域名
+function editDomain(e) {
+    let key = e.target.id
+    hash[key] = prompt('输入您的域名: ') || hash[key]
+    localStorage.setItem('localData', JSON.stringify(hash))
+}
+
+
+// 1. 初始化数据
 let keys = {
     0: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     1: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
@@ -17,24 +50,26 @@ let hash = {
     s: 'sm.ms'
 }
 
-let hashInLocalStorage = JSON.parse(localStorage.getItem('localData') || null)
+let hashInLocalStorage = getFromLocalStorage('localData')
 
 if (hashInLocalStorage) {
     hash = hashInLocalStorage
 }
 
+
+// 2. 生成键盘
 let index = 0
 while (index < keys['length']) {
-    let divWrapper = document.createElement('div')
-    divWrapper.className = "row"
+    let divWrapper = createTag('div', {className: 'row'})
     mainWrapper.appendChild(divWrapper)
 
     let i = 0
     while (i < keys[index].length) {
-        let kbdx = document.createElement('kbd')
-        let span = document.createElement('span')
-        let img = document.createElement('img')
         let textKey = keys[index][i]
+        let kbdx = createTag('kbd', {className: 'keyStyle'})
+        let span = createTag('span', {textContent: textKey})
+        let img = createTag('img')
+        
         if(hash[textKey]) {
             img.src = `http://${hash[textKey]}/favicon.ico`
         } else {
@@ -44,16 +79,8 @@ while (index < keys['length']) {
         img.onerror = function(e) {
             e.target.src = 'https://i.loli.net/2021/09/29/MhWi3CBnrYG4Hlb.png'
         }
-        span.textContent = textKey
-        kbdx.className = "keyStyle"
-        let btn = document.createElement('button')
-        btn.id = keys[index][i]
-        btn.textContent = '编辑'
-        btn.onclick = function (e) {
-            let key = e.target.id
-            hash[key] = prompt('输入您的域名: ') || hash[key]
-            localStorage.setItem('localData', JSON.stringify(hash))
-        }
+
+        let btn = createTag('button', {id: textKey, textContent: '编辑', onclick: editDomain})
 
         kbdx.appendChild(span)
         kbdx.appendChild(img)
